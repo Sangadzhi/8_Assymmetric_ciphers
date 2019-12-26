2fa0b1a0be83cab1ea2e722dc1838046a957f292
Sangadzhi <zaiasg@mail.ru>
2019-12-26 03:29:45 +0300

    Основные + 1,2,3 доп

--- client.py
+++ client.py
@@ -1,14 +1,46 @@
 import socket
-import pickle
+from DH import Endpoint
 
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 8082
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
 
-sock.close()
+clientDH = Endpoint()
+clientDH.bunch_of_public_keys()
+
+keys = str(clientDH.client_public_key)+' '+str(clientDH.server_public_key)
+sock.send(keys.encode())
+
+
+msg = sock.recv(1024).decode()
+if msg == "Access is allowed":
+    print(msg+"\nTo exit, send \"exit\"")
+
+    
+    server_key_partial = int(sock.recv(1024).decode())
+    
+
+
+    client_partial_key = clientDH.generate_partial_key()
+    sock.send(str(client_partial_key).encode())  
+
+    
+    clientDH.generate_full_key(server_key_partial)
+
+    while True:
+        msg = input(""">>""")
+        if msg == 'exit' or msg == 'Exit':
+            sock.send(clientDH.encrypt_message(msg).encode())
+            break
+        
+        sock.send(clientDH.encrypt_message(msg).encode())
+
+
+    sock.close()
+
+else:
+    print("Access not allowed")
+    sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,61 @@
+import random
 import socket
-import pickle
+from DH import Endpoint
 
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 8082
 
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
+print(f'Listen {PORT} port...')
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+def make_keys(conn):
+    
+    
+    bunch = conn.recv(2054).decode()
+    bunch = bunch.split(' ')
+    
+    serverDH = Endpoint(int(bunch[0]), int(bunch[1]), random.randint(1, 320))
+    return serverDH
 
-conn.close()
+def access_check(client_public_key):
+    
+    with open('Keys.txt', 'r') as file:
+        flag = False
+        for line in file:
+            if int(line) == client_public_key:
+                flag = True
+                break
+    return flag
+
+serverDH = make_keys(conn)
+
+if access_check(serverDH.client_public_key):
+    conn.send("Access is allowed".encode())
+
+    
+    server_partial_key = serverDH.generate_partial_key()
+    conn.send(str(server_partial_key).encode())
+
+    
+    client_key_partial = int(conn.recv(1024).decode())
+    print(client_key_partial)
+
+    
+    serverDH.generate_full_key(client_key_partial)
+
+
+    while True:
+
+        
+        msg = conn.recv(2024).decode()
+        print(f'Encrypt messege: {msg} \nDecrypt messege: {serverDH.decrypt_message(msg)}\n')
+        if serverDH.decrypt_message(msg) == 'Exit' or serverDH.decrypt_message(msg) == 'exit':
+            break
+
+    conn.close()
+
+else:
+    conn.close()
